bcb0651a9f5facc4feb7778c7a714f7cc02a182b
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-23 01:30:41 +0600

    left sidebar

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h2>LeftSideNav</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="border rounded py-4 px-3 mb-6">
+      <h2 className="text-2xl mb-3 font-bold">All Cacategories </h2>
+      {
+        categories.map(  category => <Link to={`/category/${category.id}`} className="block ml-4 text-[#403F3F] py-4 text-xl font-semibold" key={category.id}>{category.name}</Link>)
+      }
+    </div>
+  );
 };
 
 export default LeftSideNav;
